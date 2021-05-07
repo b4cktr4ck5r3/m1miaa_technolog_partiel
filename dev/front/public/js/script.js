0b8cf40c4e5f9ff08936d1558a6d0a3778677ebd
b4cktr4ck5r3 <flennertom@gmail.com>
2021-05-07 16:22:30 +0200

    add command logic (without persistance, only status code)

--- dev/front/public/js/script.js
+++ dev/front/public/js/script.js
@@ -1,5 +1,4 @@
 async function getVersion(){
-    console.log("getVersion()");
     const version = await fetch('http://localhost:3000/version')
                         .then(res => res.text())
                         .then(text => JSON.parse(text)["version"]);
@@ -7,9 +6,14 @@ async function getVersion(){
 }
 
 async function getPizzas(){
-    console.log("getPizzas()");
     const pizzas = await fetch('http://localhost:3000/pizzas')
                         .then(res => res.text())
                         .then(text => JSON.parse(text));
     return pizzas;
+}
+
+async function getPizza(name){
+    const pizza = await fetch(`http://localhost:3000/pizzas?name=${name}`)
+                        .then(res => res.status)
+    return pizza;
 }
